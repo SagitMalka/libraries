0ef9be1d0ff4ac578a2aee99fbb94f300611ae9b
Sagit Malka <118733667+SagitMalka@users.noreply.github.com>
2024-01-21 14:19:48 +0200

    Uncomment lines in main.c

--- main.c
+++ main.c
@@ -6,20 +6,20 @@ int main(){
     int startNum, endNum;
     scanf("%d %d", &startNum, &endNum);
 
-    // printf("The Armstrong numbers are:");
-    // for (int i = startNum; i < endNum; i++)
-    // {
-    //     if(isArmstrong(i) == 1){
-    //         printf("%d", i);
-    //     }
-    // }
+    printf("The Armstrong numbers are:");
+    for (int i = startNum; i < endNum; i++)
+    {
+        if(isArmstrong(i) == 1){
+            printf(" %d", i);
+        }
+    }
     printf("\n");
 
     printf("The Palindromes are:");
     for (int i = startNum; i < endNum; i++)
     {
         if(isPalindrome(i) == 1){
-            printf("%d", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -28,7 +28,7 @@ int main(){
     for (int i = startNum; i < endNum; i++)
     {
         if(isPrime(i) == 1){
-            printf("%d", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -37,7 +37,7 @@ int main(){
     for (int i = startNum; i < endNum; i++)
     {
         if(isStrong(i) == 1){
-            printf("%d", i);
+            printf(" %d", i);
         }
     }
     
