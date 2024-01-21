688e16ea5990af5af99a368b636c394652e03036
Sagit Malka <118733667+SagitMalka@users.noreply.github.com>
2024-01-21 16:14:42 +0200

    isPali

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <math.h>
 
-int power(int base, int exponent){
-int result=1;
 
-for (exponent; exponent>0; exponent--)
-{
-result = result * base;
-}
-return result;
+int power(int x, int n){
+    
+    if (n == 0)
+        return 1;
+ 
+    if (x == 0)
+        return 0;
+    return x * power(x, n - 1);
 }
 
 int countDigitsRecursive(int n){
@@ -19,29 +20,19 @@ int countDigitsRecursive(int n){
     return 1+ countDigitsRecursive(n/10);
 }
 
-// int reversedNumber(int n){
-//     int rem;
-//     static int sum = 0;
-//     if (n != 0) {
-//     rem = n % 10;
-//     sum = sum * 10 + rem;
-//     reversedNumber(n / 10);
-//     } else
-//         return sum;
-//     return sum;
-// }
-
-int isPalindrome(int n){
-    static int sum = 0;
-    if(n != 0){
-        sum = sum *10 + n % 10;
-        isPalindrome(n / 10);
-    }else if (sum == n)
-    {
-        return 1;
-    }else
+int reverse(int num) {
+    int digit = countDigitsRecursive(num);
+    if (num == 0)
         return 0;
-    
+
+    return ((num % 10 * pow(10, digit)) + reverse(num / 10));
+}
+
+int isPalindrome(int num) {
+    if (num == reverse(num)) {
+        return 1;
+    }
+    return 0;
 }
 
 int calcArmstrong(int num, int originalNum, int n) {
